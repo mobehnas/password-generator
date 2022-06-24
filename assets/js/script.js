d56d21b944b14431747d04034efc08eedbf0b2a5
mohamed behnas <behna@DESKTOP-T1K4JBI.(none)>
2022-06-24 16:36:45 -0400

    add and update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// Assigned variables
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
 var special = "!@,#$%&*{}[]/\\+=";
 var chosenCharacters = "";
 
-// Write password to the #password input
-function enterpassword() {
-  var password = genratepassword;
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
 
-//write password function 
-function writePassword(){
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
-  }
-  
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword)
-  
-  //generate password function gets called in writePassword function, and it should return the final password
   function generatePassword(){
-    //do random generation here and return the final password in the end
+
     var result = "";
-    //ask the user how many characters they want
+ 
     var length = prompt("How many characters in your random password? (between 8 and 128)");
     if(isNaN(length)){
       alert("You must input a number!");
@@ -39,7 +20,6 @@ function writePassword(){
       alert("Please choose numbers between 8 - 128!");
       return generatePassword()
     }
-    //ask the user if they want to include upper, lower, num, special?
     var hasUpper = confirm("Include upper case letters?");
     var hasLower = confirm("Include lower case letters?");
     var hasNumbers = confirm("Include numbers?");
@@ -49,18 +29,15 @@ function writePassword(){
       alert("You must at least choose 1 character type!");
       return generatePassword()
     }
-  
     if(hasUpper){
       chosenCharacters += upper
     }
     if(hasLower){
       chosenCharacters += lower
     }
-  
     if(hasNumbers) {
       chosenCharacters += numbers
     }
-  
     if(hasSpecial) {
       chosenCharacters += special
     }
@@ -69,5 +46,21 @@ function writePassword(){
     result += chosenCharacters.charAt(Math.floor(Math.random() * chosenCharacters.length));
   }
   return result;
-  
   }
+
+  // Write password to the #password input
+function enterpassword() {
+  var password = genratepassword;
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+  //write password function 
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
